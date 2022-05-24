35da47b6b0a69ca5c88ca9cd5f3a394784677636
Pascal Abou Jaoude <pascal.aboujaoude@gmail.com>
2022-05-24 09:07:01 +0300

    putting time

--- game.js
+++ game.js
@@ -1,6 +1,36 @@
 var score = 0;
 var started = 0;
 var lost = 0;
+var seconds = 00;
+var tens = 00;
+var intervalTime = [];
+var appendTens = document.getElementById("tens");
+var appendSeconds = document.getElementById("seconds");
+
+function startTime() {
+  Interval = setInterval(startTimer, 10);
+  tens++;
+
+  if (tens <= 9) {
+    appendTens.innerHTML = "0" + tens;
+  }
+
+  if (tens > 9) {
+    appendTens.innerHTML = tens;
+  }
+
+  if (tens > 99) {
+    console.log("seconds");
+    seconds++;
+    appendSeconds.innerHTML = "0" + seconds;
+    tens = 0;
+    appendTens.innerHTML = "0" + 0;
+  }
+
+  if (seconds > 9) {
+    appendSeconds.innerHTML = seconds;
+  }
+}
 
 function gameOver() {
   started = 0;
@@ -37,6 +67,12 @@ function restartGame() {
   lost = 0;
   var status = document.getElementById("status");
   status.innerHTML = "Try Again";
+
+  clearInterval(Interval);
+  tens = "00";
+  seconds = "00";
+  appendTens.innerHTML = tens;
+  appendSeconds.innerHTML = seconds;
 }
 
 window.onload = function () {
